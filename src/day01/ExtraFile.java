60dc55430b8e17af9fd133e86e120965f724e33e
Gizbu <gzmmtely@gmail.com>
2026-02-26 12:13:22 -0500

    Update ExtraFile.java

--- src/day01/ExtraFile.java
+++ src/day01/ExtraFile.java
@@ -1,6 +1,7 @@
 package day01;
 
 public class ExtraFile {
+    public static void main(String[] args){
     System.out.println("Extra file added to the remote repository");
 
     //This is extra file.
